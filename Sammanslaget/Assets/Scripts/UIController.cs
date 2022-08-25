e71d178a22321b05cbfdb4bc5e7302ae341f7604
EricLocher <eriloc1129@skola.goteborg.se>
2022-08-25 09:52:19 +0100

    Update UIController.cs

--- Sammanslaget/Assets/Scripts/UIController.cs
+++ Sammanslaget/Assets/Scripts/UIController.cs
@@ -6,15 +6,9 @@ using TMPro;
 
 public class UIController : MonoBehaviour
 {
-
-    [Header("Refrences")]
     [SerializeField] TMP_Text points, trend, queue;
     [SerializeField] Image hearts;
     [SerializeField] ArticleQueue _queue;
-
-    [Header("Animations")]
-    [SerializeField] AnimationCurve pointsAnimation;
-
 
     private void Start()
     {
@@ -35,29 +29,11 @@ public class UIController : MonoBehaviour
     void UpdatePoints(int points)
     {
         this.points.text = $"{points}";
-        StartCoroutine(Animate(.5f, pointsAnimation, this.points.GetComponent<RectTransform>()));
     }
 
     void UpdateHealth(int points)
     {
         hearts.fillAmount = GameStats.GetHealth / 3f;
-    }
-
-    IEnumerator Animate(float t, AnimationCurve animCurve, RectTransform rect)
-    {
-        Vector2 size = rect.sizeDelta;
-        rect.sizeDelta = Vector2.zero;
-
-        float timeElapsed = 0;
-
-        while (timeElapsed <= t) {
-            yield return new WaitForEndOfFrame();
-            timeElapsed += Time.deltaTime;
-
-            rect.sizeDelta = size * animCurve.Evaluate(timeElapsed / t);
-        }
-
-        yield return null;
     }
 
     void OnDestroy()
